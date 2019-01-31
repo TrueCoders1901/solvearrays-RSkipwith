45432aa7fdbbe2adbc5c9292cc93e9f79aa04df7
Rashaun Skipwith <rashaun.t.skipwith@gmail.com>
2019-01-31 15:25:53 -0600

    solved

--- SolveArrays/ArraySolver.cs
+++ SolveArrays/ArraySolver.cs
@@ -7,26 +7,66 @@ namespace SolveArrays
     {
         public int Sum(int[] numbers)
         {
-            // TODO: Implement this method
-            throw new NotImplementedException();
+            int SumOfArray = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                SumOfArray += numbers[i];
+            }
+            return SumOfArray;
         }
 
         public int SumEvens(int[] numbers)
         {
-            // TODO: Implement this method
-            throw new NotImplementedException();
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sum += numbers[i];
+                }
+            }
+            return sum;
         }
 
         public double AverageEvens(int[] numbers)
         {
-            // TODO: Implement this method
-            throw new NotImplementedException();
+            int countOfEvens = 0;
+            int totalOfEvens = 0;
+
+
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    totalOfEvens += num;
+                    countOfEvens += 1;
+                }
+            }
+            if (countOfEvens == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                double average = (double)totalOfEvens / countOfEvens;
+                return average; 
+            }
+
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            // TODO: Implement this method
-            throw new NotImplementedException();
-        }
+            int total = Sum(numbers.ToArray());
+
+            if (total % 2 == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        } 
     }
 }
+
